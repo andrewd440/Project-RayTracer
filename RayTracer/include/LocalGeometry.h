6d85b92e226b4936a9f108c4450256b5931f3d2a
Andrew Lowry <andrew_d_lowry@yahoo.com>
2014-07-23 11:40:56 -0400

    Added LocalGeometry class

---
+++ RayTracer/include/LocalGeometry.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Vector3.h"
+
+struct LocalGeometry
+{
+	/**
+	* Default constructor.
+	* point = normal = zero vector
+	*/
+	LocalGeometry()
+		: point()
+		, surfaceNormal()
+	{
+	}
+
+	/**
+	* Construct local geometry from a surface point and normal
+	* @param pointVector - a point on a geometry
+	* @param normal - normalized surface normal at the pointVector
+	*/
+	LocalGeometry(Vector3f pointVector, Vector3f normal)
+		: point(pointVector)
+		, surfaceNormal(normal)
+	{
+	}
+
+	Vector3f point;				/* Point on the surface of the geometry */
+	Vector3f surfaceNormal;		/* Surface normal at the point on the geometry */
+};
+
