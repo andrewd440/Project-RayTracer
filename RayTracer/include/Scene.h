99c1081547b5aeb98c2d7c51a455645408606722
Andrew Lowry <andrew_d_lowry@yahoo.com>
2014-07-28 12:37:42 -0400

    Added Scene class

--- RayTracer/include/Scene.h
+++ RayTracer/include/Scene.h
@@ -1,8 +1,39 @@
 #pragma once
+#include "Light.h"
+#include "Shape.h"
+#include "Camera.h"
+#include "RayTracer.h"
+
+#include <vector>
+
 class Scene
 {
 public:
 	Scene();
 	~Scene();
+
+	/**
+	* Retrieves the scene camera.
+	* @return The scene camera (unmodifiable)
+	*/
+	const Camera& getCamera() const;
+
+	/**
+	* Retrieves all shapes in the scene.
+	* @return A const vector containing the shapes.
+	*/
+	const std::vector<Shape>& getShapes() const;
+
+	/**
+	* Retrieves all lights in the scene.
+	* @return A const vector containing the lights.
+	*/
+	const std::vector<Light>& getLights() const;
+
+private:
+	RayTracer mRayTracer; /*  */
+	Camera mCamera; /* Camera for the scene */
+	std::vector<Shape> mShapes; /* All shapes in the scene */
+	std::vector<Light> mLights; /* All lights in the scene */
 };
 
